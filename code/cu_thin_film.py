03110782e8cac86d2b850b34b5378fe1d0adc68b
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-05-30 21:48:27 +0200

    starting over, now calculating number of jump sites

--- code/cu_thin_film.py
+++ code/cu_thin_film.py
@@ -5,8 +5,8 @@ import seaborn as sns
 import pandas as pd
 
 # parameters
-grid_dim_y = 10
-grid_dim_x = 10
+grid_dim_y = 4
+grid_dim_x = 4
 TotalTime = 10 
 initial_concentration = 0.1  
 cu_thickness = 100e-9  
@@ -22,13 +22,12 @@ ActivationEnergy_Cu = 134.5e3
 distance = 2.54e-10 
 R = 8.314  # general gas constant J/(mol*K)
 
-# Funktion zur Berechnung des Diffusionskoeffizienten für Cu
 def diffusion_coefficient_cu(temperature):
     return diff_coeff * np.exp(-ActivationEnergy_Cu / (R * temperature))
 
 
-def generate_cu_al_grid(N, percent_aluminum):
-    num_aluminum_atoms = int(percent_aluminum * N) # percentage of aluminum atoms
+def generate_cu_al_grid(percent_aluminum):
+    num_aluminum_atoms = int(percent_aluminum * grid_dim_x*grid_dim_y) # percentage of aluminum atoms
     
     grid = np.zeros((grid_dim_y, grid_dim_x))
 
@@ -36,92 +35,73 @@ def generate_cu_al_grid(N, percent_aluminum):
 
     return grid
 
-def get_coordinates_of_nearby_type(grid, row, col, type=1):
+def get_coordinates_of_neighbor_type(grid, row, col, type=0):
     nearby_atoms = []
-    for i in range(row-1, row+2):
-        for j in range(col-1, col+2):
-            if 0 <= i < 10 and 0 <= j < 10 and (i != row or j != col):
-                if grid[i, j] == type:
-                    nearby_atoms.append((i, j))
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    for direction in directions:
+        i = row + direction[0]
+        j = col + direction[1]
+        if 0 <= i < grid_dim_y and 0 <= j < grid_dim_x and grid[i, j] == type:
+            nearby_atoms.append((i, j))
     return nearby_atoms
 
 
-def get_coordinates_atoms_type(grid, type = 0):
+def get_coordinates_atoms_type(grid, type = 1):
     coordinates = []
-    for i in range(grid_dim_y):
-        for j in range(grid_dim_x):
-            if grid[i, j] == 0:
+    for i in range(0,grid_dim_y):
+        for j in range(0, grid_dim_x):
+            if grid[i, j] == type:
                 coordinates.append((i, j))
     return coordinates
 
-def kmc_diff_sim(TotalTime, initial_concentration, cu_thickness, al_concentration, initial_temperature, end_temperature, num_steps=1000):
-    current_time = t = 0
+def kmc_sim(TotalTime, initial_concentration, cu_thickness, al_concentration, initial_temperature, end_temperature, num_steps=1000):
+    current_time = 0
     T = initial_temperature
     current_temperature = initial_temperature
-    current_concentration_cu = np.ones(num_steps+1) * initial_concentration * 100  # 
-    delta_t = TotalTime / num_steps
     
     # Generate CuAl grid
-    lattice = generate_cu_al_grid(100, al_concentration) # 0: Cu, 1: Al
+    lattice =  np.zeros((grid_dim_y,grid_dim_x)) #generate_cu_al_grid(al_concentration) # 0: Cu, 1: Al
+    lattice[:grid_dim_y//2,:] = 1
     fig, ax = plt.subplots(figsize=(6, 6))
-    sns.heatmap(lattice, cmap='Blues' , cbar=False, square=True, ax=ax, yticklabels=True, xticklabels=True)
-    plt.show()
+    #sns.heatmap(lattice, cmap='Blues' , cbar=False, square=True, ax=ax, yticklabels=True, xticklabels=True)
+    #plt.show()
     
     neighbor_number = 4  # for 2D
+    al_coordinates = get_coordinates_atoms_type(lattice, type=1)
+    possible_jump_sites = []
+    no_possible_jump_sites = []
+    for atom_coordinates in al_coordinates:
+        row, col = atom_coordinates
+        current_possible_jump_sites = get_coordinates_of_neighbor_type(lattice, row, col, type=0)
+        no_possible_jump_sites.append(len(current_possible_jump_sites))
+        possible_jump_sites.append(current_possible_jump_sites)
+    
+    total_possible_jump_sites = sum(no_possible_jump_sites)    
+    
 
-    for i in range(num_steps):
-        cu_coordinates = get_coordinates_atoms_type(lattice)
-        # number of Cu atoms 
-        number_cu_neighbor = np.zeros(len(cu_coordinates))
-        for j, (row, col) in enumerate(cu_coordinates):
-            surroundings = get_coordinates_of_nearby_type(lattice, row, col, type=1)
-            number_cu_neighbor[j] = neighbor_number - len(surroundings)
-        
-        # jump rates
-        k_cu = diffusion_coefficient_cu(current_temperature)
-        k_cu_mat = number_cu_neighbor * k_cu
-        
-        # loop over Cu atoms and check if they jump back or diffuse
-        for j, (row, col) in enumerate(cu_coordinates):
-            k_diffusion = k_cu_mat[j]
-            k_reversion = neighbor_number * diffusion_coefficient_cu(current_temperature)
-            r_diffusion = random.uniform(0, 1)
-            r_reversion = random.uniform(0, 1)
+
+
             
-            # check if Cu atom diffuses or reverts
-            if r_diffusion < k_diffusion / (k_diffusion + k_reversion):
-                jump_direction = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1)])
-                new_position = ((row + jump_direction[0]) % 10, (col + jump_direction[1]) % 10)
-                
-                lattice[row, col] = 1
-                lattice[new_position] = 0
-                # update concentration
-                current_concentration_cu[i+1] = current_concentration_cu[i]
-            elif r_reversion < k_reversion / (k_diffusion + k_reversion):
-                lattice[row, col] = 'Al'
-                # Aktualisiere die Konzentration für Nicht-Schritte
-                current_concentration_cu[i+1] = current_concentration_cu[i] + 0.01
+    # update time
+    t_ij = -np.log(random.uniform(0, 1)) / np.sum(k_cu_mat)
+    t += t_ij
+    vT = R * T  
+    T += vT * t_ij
         
-        # update time
-        t_ij = -np.log(random.uniform(0, 1)) / np.sum(k_cu_mat)
-        t += t_ij
-        vT = R * T  
-        T += vT * t_ij
-        
-        # update temperature
-        current_temperature = initial_temperature + (end_temperature - initial_temperature) * (current_time / TotalTime)
-        current_time += delta_t
+    # update temperature
+    current_temperature = initial_temperature + (end_temperature - initial_temperature) * (current_time / TotalTime)
+    current_time += delta_t
     
     time = np.linspace(0, TotalTime, num_steps+1)
-    return time, current_concentration_cu
+    return time, atoms
 
 
 if __name__ == '__main__':
-    time, concentration_cu = kmc_diff_sim(TotalTime, initial_concentration, cu_thickness, al_concentration, initial_temperature, end_temperature, num_steps)
+    time, atoms = kmc_sim(TotalTime, initial_concentration, cu_thickness, al_concentration, initial_temperature, end_temperature, num_steps)
 
     # Ergebnisse plotten
     plt.figure(figsize=(10, 6))
-    plt.plot(time, concentration_cu, color='pink', linestyle='x', label='Cu concentration')
+    plt.plot(time, concentration_cu, color='pink', linestyle='--', label='Cu concentration')
     plt.axhline(y=initial_concentration * 100, color='red', linestyle='--', label='Anfangskonzentration Cu')
     plt.title('Entwicklung der Kupferkonzentration in CuAl-Dünnschicht')
     plt.legend()
